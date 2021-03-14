3dcc23052bf0975c503b750009e74cb0f48b8fa6
NiniLiao <u9513031@cmu.edu.tw>
2021-03-14 20:56:25 +0800

    added TestRouterForNonExistentRoute

--- main_test.go
+++ main_test.go
@@ -59,3 +59,30 @@ func TestRouter(t *testing.T) {
 		t.Errorf("Response should be %s, got %s", expected, respString)
 	}
 }
+
+func TestRouterForNonExistentRoute(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+	resp, err := http.Post(mockServer.URL+"/hello", "", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status should be 405, got %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	respString := string(b)
+	expected := ""
+
+	if respString != expected {
+		t.Errorf("Response should be %s, got %s", expected, respString)
+	}
+
+}
